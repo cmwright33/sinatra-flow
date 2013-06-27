6a1f5efdeebdfe0f87a7031131f519de31cd82d1
Christopher Wright <cmwright33@gmail.com>
2013-06-27 11:57:30 -0400

    updated file

--- main.rb
+++ main.rb
@@ -13,5 +13,30 @@ require 'sinatra/reloader' if development?
 configure do
 end
 
-get '/' do
+post '/new_color' do
+  # because ive posted parameters here i can access them in the param hash
+  # in the form of the /colors view, i sathe the name of the parameters is "color"
+  $global_colors << params[:color]
+  redirect to('/colors')
 end
+
+
+
+get '/colors' do
+  # This is the colors route
+  # This would be correspond to the colors 'view'
+  # we will create a colors.erb file in the views/ directory
+
+  #this lime tells us to use the colors.erb view
+  # any instance variables in this route (get '/colors')
+  # will be avaliable in the colors view
+  #local variable from this route however
+  # are not avaliable in view
+  # a variable like foo is not avaliable
+  # a varibale like @foo is abvaliable
+
+  $global_colors = ['Red', 'Green', 'Blue', 'Purple']
+  erb :colors
+end
+
+@colors = $global_colorss
